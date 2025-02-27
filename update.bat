389dcb4b57a03531ae9dd63ddfa799398f392879
Curt Carpenter <curt.carpenter@live.com>
2025-02-26 19:58:56 -0800

    Try again

--- update.bat
+++ update.bat
@@ -1,1 +1,6 @@
-butler push WebExport\index.zip curtc/excape:HTML5
+del /q "D:\Godot Demos\Godot Exports\Excape\*"
+C:\Godot\Godot_v4.3-stable_win64.exe\Godot_v4.3-stable_win64.exe --export-release "Web" "D:\Godot Demos\Godot Exports\Excape\index.html"
+pushd "D:\Godot Demos\Godot Exports\Excape"
+tar -a -c -f index.zip *
+butler push index.zip curtc/excape:HTML5
+popd
